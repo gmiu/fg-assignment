69d2716a05452d33ea6b155fdad8de7740f78ea8
Gabriel Miu <gmiu@gbrlm.com>
2024-02-16 18:15:06 +0200

    [Q2] Remove old infra code

--- question2/infra_old/bastion/data.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-
-  config = {
-    region = var.aws_region
-    bucket = "197490477086-tech-gbrlm-tfstate"
-    key    = "fg-assignment/vpc.tfstate"
-  }
-}
--- question2/infra_old/bastion/output.tf
+++
@@ -1,7 +0,0 @@
-output "bastion_public_ip" {
-  value = module.bastion.public_ip
-}
-
-output "bastion_sg_id" {
-  value = module.bastion-sg.security_group_id
-}
