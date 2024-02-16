42fd390ff64ea32d6de702304b435253f0f34825
Gabriel Miu <gmiu@gbrlm.com>
2024-02-16 03:24:53 +0200

    [Q2] Fix terraform fmt

--- question2/infrastructure/modules/alb/outputs.tf
+++ question2/infrastructure/modules/alb/outputs.tf
@@ -1,14 +1,14 @@
-output alb_security_group_id {
-  value = module.alb.security_group_id
+output "alb_security_group_id" {
+  value       = module.alb.security_group_id
   description = "The ID of the security group for the ALB"
 }
 
-output alb_target_group_http_arn {
-  value = module.alb.target_groups.http.arn
+output "alb_target_group_http_arn" {
+  value       = module.alb.target_groups.http.arn
   description = "The ARN of the HTTP target group"
 }
 
-output alb_dns_name {
-  value = module.alb.dns_name
+output "alb_dns_name" {
+  value       = module.alb.dns_name
   description = "The DNS name of the ALB"
 }
